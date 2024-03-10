2635b19f685a39827de5e4b8a2699dd3e2b50b21
finisus <finisvisus@gmail.com>
2024-03-11 03:47:49 +0530

    push mobile optimization

--- app.js
+++ app.js
@@ -14,4 +14,24 @@ document.addEventListener("DOMContentLoaded", function() {
   twitterBtn.addEventListener('click', function() {
     window.open('https://x.com/', '_blank');
   });
+
+  function checkScreenWidth() {
+    const mobileImages = document.querySelectorAll("#backdrop img[id^='mobile-back-']"); // Using #backdrop here
+    const otherImages = document.querySelectorAll("#backdrop img:not([id^='mobile-back-'])");
+  
+    if (window.innerWidth <= 659) {
+      mobileImages.forEach(image => image.style.display = "block"); /* Added missing closing parenthesis */
+      otherImages.forEach(image => image.style.display = "none");
+    } else {
+      mobileImages.forEach(image => image.style.display = "none");
+      otherImages.forEach(image => image.style.display = "block");
+    }
+  }
+  
+  // Call the function on initial page load
+  checkScreenWidth();
+  
+  // Listen for window resize events and call the function again
+  window.addEventListener("resize", checkScreenWidth);
+
 });
